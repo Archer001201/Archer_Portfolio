ddfa0b9e98159e7017a84034a3f3f5ac9ec4ee85
Archer <zhongchuqiao1201@gmail.com>
2023-07-23 14:47:57 -0700

    很多

---
+++ js/carousel.js
@@ -0,0 +1,57 @@
+let currentImageIndex = 2;
+const images = Array.from(document.querySelectorAll('.carousel-image'));
+const carousel = document.querySelector('.carousel-images');
+const prevButton = document.querySelector('.carousel-button-prev');
+const nextButton = document.querySelector('.carousel-button-next');
+const currentName = document.querySelector('.current-image-name');
+const currentTag = document.querySelector('.current-image-tag');
+const currentDate = document.querySelector('.current-image-date');
+
+function updateCarousel() {
+    carousel.style.transform = `translateX(-${currentImageIndex * 300}px)`;
+    carousel.querySelectorAll('.carousel-image').forEach((image, index) => {
+        if (index === currentImageIndex + 1) {
+            image.classList.add('center');
+        } else {
+            image.classList.remove('center');
+        }
+    });
+    currentName.textContent = images[currentImageIndex + 1].querySelector('.image-name').textContent;
+    currentTag.textContent = images[currentImageIndex + 1].querySelector('.image-tag').textContent;
+    currentDate.textContent = images[currentImageIndex + 1].querySelector('.image-date').textContent;
+}
+
+updateCarousel();
+
+prevButton.addEventListener('click', () => {
+    if (currentImageIndex > 1) {
+        currentImageIndex--;
+    } else {
+        carousel.style.transition = 'none';
+        currentImageIndex = images.length - 3;
+    }
+    setTimeout(updateCarousel, 0);
+    setTimeout(() => { carousel.style.transition = ''; }, 0);
+});
+
+nextButton.addEventListener('click', () => {
+    if (currentImageIndex < images.length - 3) {
+        currentImageIndex++;
+    } else {
+        carousel.style.transition = 'none';
+        currentImageIndex = 1;
+    }
+    setTimeout(updateCarousel, 0);
+    setTimeout(() => { carousel.style.transition = ''; }, 0);
+});
+
+images.forEach(function (link) {
+    link.addEventListener('click', function (event) {
+        event.preventDefault();
+        link.classList.add('move');
+        link.addEventListener('animationend', function () {
+            link.classList.remove('move');
+            window.location.href = link.href;
+        }, { once: true });
+    });
+});
--- js/page_piling.js
+++ js/page_piling.js
@@ -9,7 +9,6 @@ $(document).ready(function() {
       
         // 计算滚动的距离
         var scrollDistance = e.originalEvent.deltaY * 0.5;
-      
         // 更新页面的位置
         updatePagePosition(scrollDistance);
     });
@@ -58,7 +57,7 @@ $(document).ready(function() {
         totalScrollTop += scrollDistance;
       
         // 限制总滚动距离在 0 到 page-pilings 的高度之间
-        totalScrollTop = Math.max(0, Math.min(totalScrollTop, pagePilings.length * vh));
+        totalScrollTop = Math.max(0, Math.min(totalScrollTop, pagePilings.length * vh-100));
       
         // 计算当前应该显示的 page-piling 索引
         var currentIndex = Math.floor(totalScrollTop / vh);
@@ -93,7 +92,6 @@ $(document).ready(function() {
                 }
             }
         });
-
     }
 });
 
